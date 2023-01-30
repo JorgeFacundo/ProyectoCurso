f815ef51aac9961938417a717b27b677a4eff8a9
Facu lpda <facu.lpda.lopz@gmail.com>
2023-01-30 12:41:05 -0300

    inicio del proyecto

---
+++ Proyecto.js
@@ -0,0 +1,24 @@
+document.getElementById('nacionalidad').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Argentino";
+}
+document.getElementById('edad').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "33 años";
+}
+document.getElementById('cumpleaños').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "9/5/1989";
+}
+document.getElementById('telefono').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "123456789";
+}
+document.getElementById('direccion').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Calle Falsa 123";
+}
+document.getElementById('email').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "everett.frazier@example.com";
+}
